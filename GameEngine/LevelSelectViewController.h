90d5a763b80568728c4b2e2035e90705fcdf7e5c
a0105750@nus.edu.sg <gujunchao@soctf-imac-034.comp.nus.edu.sg>
2014-02-17 01:25:34 +0800

    level designer

---
+++ GameEngine/LevelSelectViewController.h
@@ -0,0 +1,13 @@
+//
+//  LevelSelectViewController.h
+//  Bubble Blast Saga
+//
+//  Created by Gu Junchao on 2/17/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LevelSelectViewController : UIViewController
+
+@end
---
+++ GameEngine/LevelSelectViewController.m
@@ -0,0 +1,39 @@
+//
+//  LevelSelectViewController.m
+//  Bubble Blast Saga
+//
+//  Created by Gu Junchao on 2/17/14.
+//
+//
+
+#import "LevelSelectViewController.h"
+
+@interface LevelSelectViewController ()
+
+@end
+
+@implementation LevelSelectViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+    self.navigationController.navigationBarHidden = YES;
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
